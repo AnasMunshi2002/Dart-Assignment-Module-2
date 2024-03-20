c5f69ee630d6f7881d3981450ae04ad74591c8c3
AnasMunshi <anasmunshi2002@gmail.com>
2024-03-20 13:57:30 +0530

    Update que_46.dart

--- que_46.dart
+++ que_46.dart
@@ -3,26 +3,44 @@
 import 'dart:io';
 
 void main(){
-  List<String> names = ['Anas','Harsh','Rupin','Jaimin','Rahil','Manav'];
+  List<String> names = [];
 
-  stdout.write('Enter the index to get the string : ');
-  int index=int.parse(stdin.readLineSync().toString());
+  stdout.write('Enter number of data in list to enter : ');
+  var size = int.parse(stdin.readLineSync().toString());
 
-  try {
-    print('String at $index in list = ${names[index]}');
-
-    stdout.write('Enter name to check the existence in list : ');
-    String name = stdin.readLineSync().toString();
-
-    if(names.contains(name)){
-      print('Names list contains the entered name ');
-    }
-    else{
-      print('The entered name is not present ');
-    }
+  for(int i =0;i<size;i++){
+    stdout.write('Enter ${i+1}st name : ');
+    var name = stdin.readLineSync().toString();
     names.add(name);
-    print(names);
-  }on RangeError{
-    print('Invalid index number');
+  }
+  print('Name in List ');
+  print(names);
+
+  stdout.write('Do you want to delete any names from list : \n');
+  print('''
+  1. Yes
+  2. No
+  ''');
+  int choice = int.parse(stdin.readLineSync().toString());
+
+  if(choice==1){
+    stdout.write('Enter name you want to delete from $names : ');
+    String remName = stdin.readLineSync().toString();
+    names.removeWhere((element) => element == remName);
+  }
+  else{
+    print('No name is deleted.');
+  }
+
+  print(names);
+
+  stdout.write('Enter a name to search : ');
+  var searchName=stdin.readLineSync().toString();
+
+  if(names.contains(searchName)) {
+    print('$searchName is present in Name list');
+  }
+  else{
+    print('$searchName is not present in Name list');
   }
 }
