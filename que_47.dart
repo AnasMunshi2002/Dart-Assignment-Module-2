926d147db2e391000e6a5b50ac5d9dfdffcac708
AnasMunshi <anasmunshi2002@gmail.com>
2024-03-20 14:54:00 +0530

    Create que_47.dart

---
+++ que_47.dart
@@ -0,0 +1,18 @@
+void main() {
+  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine'};
+
+  var elements = <String>{};
+  elements.add('fluorine');
+  elements.addAll(halogens);
+  print('Set as set : $elements');
+  print('Set as List : ${elements.toList()}');
+
+  elements.removeWhere((element) => element.toString().toLowerCase().contains('or'.toLowerCase()));
+
+  print(elements);
+
+  Set<String> alkalines = {'Lithium', 'Magnesium','Sodium'};
+
+  var union = elements.union(alkalines).toSet();
+  print(union);
+  }
