55e9d478c5c6aad3da522cec873e2cb82614fcb3
AnasMunshi <anasmunshi2002@gmail.com>
2024-04-03 15:34:46 +0530

    Create que_48.dart

---
+++ que_48.dart
@@ -0,0 +1,24 @@
+void main(){
+
+  var studentMap = <String,String>{};
+
+  studentMap['RollNo']= '052';
+  studentMap['Name']= 'Anas';
+  studentMap['Dept']= 'Flutter';
+
+  print(studentMap);
+  studentMap.addAll({'Phone': '7229096366','Grade':'B+'});
+
+  print(studentMap);
+  studentMap.removeWhere((key, value) => key.toLowerCase().contains('d'.toLowerCase()));
+  print(studentMap);
+
+  studentMap['Dept'] = 'Flutter';
+  studentMap.update('Dept', (value) => 'Computer');
+
+  print(studentMap);
+
+  studentMap.clear();
+  print(studentMap);
+  
+}
